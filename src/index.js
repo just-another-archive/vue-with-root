1750444956ab88e26a4b1885548ea2d26bd33d8b
Julien Barbay <barbay.julien@gmail.com>
2018-10-24 15:36:51 +0900

    upgrade build to support esm

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import component from './component'
 
-export default {
-  component,
-  Plugin: {
-    install: function(Vue) {
-      Vue.component('with-root', component)
-    }
+export const WithRoot = component
+
+export const Plugin = {
+  install: function(Vue) {
+    Vue.component('with-root', component)
   }
+}
+
+export default {
+  WithRoot, Plugin
 }
